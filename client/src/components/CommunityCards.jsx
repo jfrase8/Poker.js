91a9c402e4a9490f874f4272d30f87314d36c139
Joel Frase <37312935+jfrase8@users.noreply.github.com>
2024-04-09 14:28:44 -0500

    Update CommunityCards.jsx

--- client/src/components/CommunityCards.jsx
+++ client/src/components/CommunityCards.jsx
@@ -7,7 +7,10 @@ const CommunityCards = (props) => {
     return(
         <>
             <div className="communityCards">
-                {this.props.cards.map((card, index) => (<Card key={index} suit={card.suit} value={card.value} background={card.imageURL}/>))}
+                {this.props.flop.map((card, index) => (<Card key={index} suit={card.suit} value={card.value} background={card.imageURL}/>))}
+                {<Card key={index} suit={this.props.turnCard.suit} value={this.props.turnCard.value} background={this.props.turnCard.imageURL}/>}
+                {<Card key={index} suit={this.props.riverCard.suit} value={this.props.riverCard.value} background={this.props.riverCard.imageURL}/>}
+                {}
             </div>
         </>
     );
